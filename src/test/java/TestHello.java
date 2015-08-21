93331324de28499fc4f47fbcd6e56fccbeea9ccb
vdahuja <vdahuja@guidewire.com>
2015-08-21 14:19:40 -0700

    Initial commit - Gradle runs tests

---
+++ src/test/java/TestHello.java
@@ -0,0 +1,31 @@
+package test.java;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class TestHello {
+
+    @Test
+    public void testPasses() {
+        String expected = "Hello, JUnit!";
+        String hello = "Hello, JUnit!";
+        assertEquals(hello, expected);
+    }
+
+    @Test
+    public void testFails() {
+        // The worlds most obvious bug:
+//        assertTrue(false);
+    }
+
+    @Test
+    public void testArray() {
+        int [] array1 = new int[] {1, 2, 3};
+        int [] array2 = new int[] {1, 2, 3};
+        assertArrayEquals(array1, array2);
+    }
+}
